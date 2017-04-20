8cf9b1486c2c18509b12e422555319838a2e9db5
paulovlobato <pvlobato@gmail.com>
2017-04-20 17:38:59 -0300

    Minor changes

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,4 @@
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-// Instantiating an angular module, and naming it as starter
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('stockMarketApp', ['ionic', 'stockMarketApp.controllers'])
 
 //Provides information about the device in which the app is running
 .run(function($ionicPlatform) {
